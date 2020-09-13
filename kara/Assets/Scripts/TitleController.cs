1a409f1c3df7d49a0c21a517901d042cb18a3f14
黒田亮介 <>
2020-09-13 16:29:29 +0900

    no message

---
+++ kara/Assets/Scripts/TitleController.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TitleController : MonoBehaviour
+{
+    [SerializeField] PopButton button;
+
+    void Start()
+    {
+        
+    }
+
+}
